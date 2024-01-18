6066d82fb31e3b33c4d7b25cdf880b8b3fb54c81
苏向夜 <fu050409@163.com>
2024-01-18 14:51:53 +0800

    :bug: fix(deps): fix path error

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,20 +1,28 @@
 from ipm import api
 
+import shutil
+
 
 def test_new():
     api.new("test")
+    shutil.rmtree("test", ignore_errors=True)
 
 
 def test_build():
     api.new("test")
     api.build("test")
+    shutil.rmtree("test", ignore_errors=True)
 
 
 def test_extract():
+    api.new("test")
     api.build("test")
-    api.extract("test\\dist\\test-0.1.0.ipk")
+    api.extract("./test/dist/test-0.1.0.ipk")
+    shutil.rmtree("test", ignore_errors=True)
 
 
 def test_install():
+    api.new("test")
     api.build("test")
-    api.install("test\\dist\\test-0.1.0.ipk")
+    api.install("./test/dist/test-0.1.0.ipk")
+    shutil.rmtree("test", ignore_errors=True)
