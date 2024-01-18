9036bf9638f3c69dcd5f25501a58bb8fa0de1712
苏向夜 <fu050409@163.com>
2024-01-18 14:16:15 +0800

    :sparkles: fear(logging): new rror method

--- src/ipm/logging.py
+++ src/ipm/logging.py
@@ -4,13 +4,17 @@ from .const import DEBUG
 logger = multilogger(name="IPM", level="DEBUG" if DEBUG else "INFO", notime=True)
 
 
-def info(message: str, echo: bool = True) -> None:
+def info(message: str, echo: bool = False) -> None:
     return logger.info(message) if echo else None
 
 
-def success(message: str, echo: bool = True) -> None:
+def success(message: str, echo: bool = False) -> None:
     return logger.success(message) if echo else None
 
 
-def error(message: str, echo: bool = True) -> None:
+def warning(message: str, echo: bool = False) -> None:
+    return logger.warning(message) if echo else None
+
+
+def error(message: str, echo: bool = False) -> None:
     return logger.error(message) if echo else None
