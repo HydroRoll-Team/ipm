0652bcf5bfbc2d6deb7d69c6c2bd4025a0d7e0fb
苏向夜 <fu050409@163.com>
2024-03-17 19:32:36 +0800

    test(api): restore api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -8,31 +8,20 @@ def test_new():
     shutil.rmtree("test", ignore_errors=True)
 
 
-# def test_build():
-#     api.new("test")
-#     api.build("test")
-#     shutil.rmtree("test", ignore_errors=True)
+def test_build():
+    api.new("test")
+    api.build("test")
+    shutil.rmtree("test", ignore_errors=True)
 
 
-# def test_extract():
-#     api.new("test")
-#     api.build("test")
-#     api.extract("./test/dist/test-0.1.0.ipk")
-#     shutil.rmtree("test", ignore_errors=True)
+def test_extract():
+    api.new("test")
+    api.build("test")
+    api.extract("./test/dist/test-0.1.0.ipk")
+    shutil.rmtree("test", ignore_errors=True)
 
 
-# def test_install():
-#     api.new("test")
-#     api.build("test")
-#     api.install("./test/dist/test-0.1.0.ipk")
-#     shutil.rmtree("test", ignore_errors=True)
-
-
-# def test_uninstall():
-#     api.uninstall("test", is_confirm=True)
-
-
-# def test_check():
-#     api.new("test")
-#     api.check("test")
-#     shutil.rmtree("test", ignore_errors=True)
+def test_check():
+    api.new("test")
+    api.check("test")
+    shutil.rmtree("test", ignore_errors=True)
